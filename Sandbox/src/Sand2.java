0e9e38cdc09de66532a3916740c09944d6d66b86
Wonch <Robert.Wonch@frontrange.edu>
2025-10-07 17:45:25 -0600

    Loops Again

---
+++ Sandbox/src/Sand2.java
@@ -0,0 +1,17 @@
+
+public class Sand2 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		printer(num);
+
+	}
+
+	private static void printer(int num) {
+		System.out.println("I have " + num + " number!");
+		
+	}
+
+	
+}
